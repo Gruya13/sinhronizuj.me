e5c34bf46e3b160af3daef15d53bef86ee2e5445
Gruya13 <gruya13@example.com>
2026-04-21 09:45:25 +0200

    Uklanjanje player_client limita kako bi yt-dlp dobio formate

--- backend/worker/downloader.py
+++ backend/worker/downloader.py
@@ -28,12 +28,6 @@ def download_youtube_video(url: str) -> dict:
         }],
         'quiet': False,
         'no_warnings': True,
-        # Bypassing YouTube Bot detection
-        'extractor_args': {
-            'youtube': {
-                'player_client': ['android', 'web']
-            }
-        },
         'nocheckcertificate': True,
         'geo_bypass': True,
         'cookiefile': 'cookies.txt', # Apsolutno 100% resenje za bot detekciju
